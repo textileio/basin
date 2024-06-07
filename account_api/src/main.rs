ab0ba0b0cf3e87da7484769b8c370e1a71c007e0
Dan Buchholz <dbuchholz30@gmail.com>
2024-06-06 23:04:34 -0500

    refactor: api handlers | use path vs params

--- account_api/src/main.rs
+++ account_api/src/main.rs
@@ -1,3 +1,4 @@
+use std::convert::Infallible;
 use std::env;
 use std::error::Error;
 
@@ -7,27 +8,38 @@ use adm_signer::{key::parse_secret_key, AccountKind, Wallet};
 use dotenv::dotenv;
 use ethers::prelude::TransactionReceipt;
 use fvm_shared::econ::TokenAmount;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
+use serde_json::json;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use warp::{Filter, Rejection, Reply};
+use warp::{http::StatusCode, Filter, Rejection, Reply};
 
-/// Request for registering or funding an account.
-#[derive(Deserialize)]
-struct AccountRequest {
-    /// Hex-prefixed public key (EVM-style address).
-    address: String,
-}
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
 
-/// Response for registering or funding an account.
-#[derive(Serialize)]
-struct AccountResponse {
-    /// Status of the request.
-    status: String,
-    /// Message for the request.
-    message: String,
-    /// Transaction receipt for the request.
-    tx: Option<TransactionReceipt>,
+    let state = Arc::new(Mutex::new(State::new()));
+
+    let register = warp::post()
+        .and(warp::path!(Network / "register" / String))
+        .and(with_state(state.clone()))
+        .and_then(handle_register);
+    let fund = warp::post()
+        .and(warp::path!(Network / "fund" / String))
+        .and(with_state(state.clone()))
+        .and_then(handle_fund);
+
+    let router = register
+        .or(fund)
+        .with(
+            warp::cors()
+                .allow_any_origin()
+                .allow_headers(vec!["Content-Type"])
+                .allow_methods(vec!["POST"]),
+        )
+        .recover(handle_rejection);
+
+    warp::serve(router).run(([127, 0, 0, 1], 8081)).await;
 }
 
 /// State for the API
@@ -42,27 +54,38 @@ impl State {
     }
 }
 
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
+/// Generic request error.
+#[derive(Clone, Debug)]
+struct BadRequest {
+    message: String,
+}
 
-    let state = Arc::new(Mutex::new(State::new()));
+impl warp::reject::Reject for BadRequest {}
 
-    let register = warp::post()
-        .and(warp::path!(Network / "register"))
-        .and(with_state(state.clone()))
-        .and(warp::body::json())
-        .and_then(handle_register);
+/// Custom error message with status code.
+#[derive(Clone, Debug, Serialize)]
+struct ErrorMessage {
+    code: u16,
+    message: String,
+}
 
-    let fund = warp::post()
-        .and(warp::path!(Network / "fund"))
-        .and(with_state(state.clone()))
-        .and(warp::body::json())
-        .and_then(handle_fund);
+/// Rejection handler for the API.
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
+    let (code, message) = if err.is_not_found() {
+        (StatusCode::NOT_FOUND, "Not Found".to_string())
+    } else if let Some(e) = err.find::<BadRequest>() {
+        let err = e.to_owned();
+        (StatusCode::BAD_REQUEST, err.message)
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, format!("{:?}", err))
+    };
 
-    let routes = register.or(fund).recover(handle_reject);
+    let reply = warp::reply::json(&ErrorMessage {
+        code: code.as_u16(),
+        message,
+    });
 
-    warp::serve(routes).run(([127, 0, 0, 1], 8081)).await;
+    Ok(warp::reply::with_status(reply, code))
 }
 
 /// Get the network configuration based on the API path parameter.
@@ -72,56 +95,52 @@ fn get_network(network: &Network) -> Result<&'static Network, &'static str> {
         // TODO: if a parent-child subnet setup is possible, then localnet or
         // devnet can also benefit from this API. Also, future mainnet, too.
         // Network::Mainnet => Ok(Network::Mainnet.init()),
-        // Network::Localnet => Network::Localnet.init(),
-        // Network::Devnet => Network::Devnet.init(),
+        // Network::Localnet => Ok(Network::Localnet.init()),
+        // Network::Devnet => Ok(Network::Devnet.init()),
         _ => return Err("Invalid network"),
     };
 }
 
 /// Get the admin wallet for sending transactions to register or fund accounts.
 fn get_admin_wallet(network: &Network) -> anyhow::Result<Wallet> {
-    let env_pk = match env::var("ACCOUNT_API_PRIVATE_KEY") {
+    let env_pk = match env::var("ADMIN_TESTNET_PRIVATE_KEY") {
         Ok(pk) => pk,
-        Err(_) => return Err(anyhow::anyhow!("ACCOUNT_API_PRIVATE_KEY not set")),
+        Err(_) => return Err(anyhow::anyhow!("ADMIN_TESTNET_PRIVATE_KEY not set")),
     };
     let pk = parse_secret_key(&env_pk)?;
     let signer = Wallet::new_secp256k1(pk, AccountKind::Ethereum, network.subnet_id()?.parent()?)?;
     Ok(signer)
 }
 
-/// Filter to pass the state to the request handlers
+/// Filter to pass the state to the request handlers.
 #[allow(unused_variables)]
 fn with_state(
     state: Arc<Mutex<State>>,
-) -> impl Filter<Extract = (Arc<Mutex<State>>,), Error = std::convert::Infallible> + Clone {
+) -> impl Filter<Extract = (Arc<Mutex<State>>,), Error = Infallible> + Clone {
     warp::any().map(move || state.clone())
 }
 
-/// Handles the `/<network>/register` request.
+/// Handles the `/<network>/register/<address>` request.
 // TODO: this creates an account on the rootnet (Filecoin) but not the subnet...
 // so is it worthwhile keeping, or should we just have the `fund` endpoint?
 #[allow(unused_variables)]
 async fn handle_register(
     network: Network,
+    address: String,
     state: Arc<Mutex<State>>,
-    req: AccountRequest,
 ) -> Result<impl Reply, Rejection> {
-    match register(&req.address, &network).await {
-        Ok(tx) => Ok(warp::reply::json(&AccountResponse {
-            status: "success".to_string(),
-            message: "Account registered on network".to_string(),
-            tx: Some(tx),
-        })),
-        Err(e) => Ok(warp::reply::json(&AccountResponse {
-            status: "error".to_string(),
-            message: e.to_string(),
-            tx: None,
-        })),
-    }
+    let res = register(&address, &network).await.map_err(|e| {
+        Rejection::from(BadRequest {
+            message: format!("register error: {}", e),
+        })
+    })?;
+
+    let json = json!({"tx": res});
+    Ok(warp::reply::json(&json))
 }
 
 /// Registers an account on Filecoin rootnet (via `InvokeEVM`), creating an
-/// f410 address for the EVM address by sending 0 FIL to it.
+/// delegate address for the EVM address by sending 0 FIL to it.
 async fn register(
     address: &str,
     network: &Network,
@@ -131,36 +150,41 @@ async fn register(
     let net = get_network(network)?;
     let admin = get_admin_wallet(net)?;
 
-    let tx = Account::transfer(
+    match Account::transfer(
         &admin,
         parse_address(address)?,
         net.parent_subnet_config(Default::default())?,
         TokenAmount::from_whole(0),
     )
-    .await?;
-
-    Ok(tx)
+    .await
+    {
+        Ok(tx) => Ok(tx),
+        // TODO: although `InvokeEVM` is called to create the delegate address,
+        // this method never returns a receipt, so we catch the error here. Is
+        // this because it's doing a transfer of 0 FIL, which is not a valid
+        // transfer? Or, perhaps it's the subnet transfer that's invalid?
+        Err(e) if e.to_string() == "transfer did not return receipt" => {
+            Ok(TransactionReceipt::default())
+        }
+        Err(e) => Err(e.into()),
+    }
 }
 
-/// Handles the `/<network>/fund` request.
+/// Handles the `/<network>/fund/<address>` request.
 #[allow(unused_variables)]
 async fn handle_fund(
     network: Network,
+    address: String,
     state: Arc<Mutex<State>>,
-    req: AccountRequest,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    match fund(&req.address, &network).await {
-        Ok(tx) => Ok(warp::reply::json(&AccountResponse {
-            status: "success".to_string(),
-            message: "Account funded successfully".to_string(),
-            tx: Some(tx),
-        })),
-        Err(e) => Ok(warp::reply::json(&AccountResponse {
-            status: "error".to_string(),
-            message: e.to_string(),
-            tx: None,
-        })),
-    }
+    let res = fund(&address, &network).await.map_err(|e| {
+        Rejection::from(BadRequest {
+            message: format!("fund error: {}", e),
+        })
+    })?;
+
+    let json = json!({"tx": res});
+    Ok(warp::reply::json(&json))
 }
 
 /// Funds an account on the subnet (sends 1 FIL), thus, initializing it on both
@@ -183,19 +207,4 @@ async fn fund(
     .await?;
 
     Ok(tx)
-}
-
-/// Custom rejection handler for the API.
-async fn handle_reject(err: Rejection) -> Result<impl Reply, Rejection> {
-    if err.is_not_found() {
-        Ok(warp::reply::with_status(
-            "404 not found",
-            warp::http::StatusCode::NOT_FOUND,
-        ))
-    } else {
-        Ok(warp::reply::with_status(
-            "Internal server error",
-            warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-        ))
-    }
 }
