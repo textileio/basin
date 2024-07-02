a7cf207b654c95390a30349909a65bab36457bad
Dan Buchholz <dbuchholz30@gmail.com>
2024-07-02 12:55:24 -0500

    refactor(faucet): impl clap instead of raw binary

--- faucet/src/main.rs
+++ faucet/src/main.rs
@@ -1,139 +1,41 @@
-use std::convert::Infallible;
-use std::env;
-use std::error::Error;
+use clap::Parser;
+use fendermint_crypto::SecretKey;
+use stderrlog::Timestamp;
 
-use dotenv::dotenv;
-use ethers::prelude::TransactionReceipt;
-use fvm_shared::{address::Address, econ::TokenAmount};
-use serde::{Deserialize, Deserializer, Serialize};
-use serde_json::json;
-use warp::{http::StatusCode, Filter, Rejection, Reply};
+use adm_signer::key::parse_secret_key;
 
-use adm_provider::util::parse_address;
-use adm_sdk::{account::Account, network::Network};
-use adm_signer::{key::parse_secret_key, AccountKind, Wallet};
+use crate::server::handle_server;
+
+mod server;
+
+#[derive(Clone, Debug, Parser)]
+#[command(name = "adm_faucet", author, version, about, long_about = None)]
+struct Cli {
+    /// Wallet private key (ECDSA, secp256k1) for sending faucet funds.
+    #[arg(short, long, env, value_parser = parse_secret_key)]
+    faucet_private_key: SecretKey,
+    /// Faucet HTTP server port.
+    #[arg(long, env, default_value("8081"))]
+    faucet_port: Option<u16>,
+    /// Logging verbosity (repeat for more verbose logging).
+    #[arg(short, long, env, action = clap::ArgAction::Count)]
+    verbosity: u8,
+    /// Silence logging.
+    #[arg(short, long, env, default_value_t = false)]
+    quiet: bool,
+}
 
 #[tokio::main]
-async fn main() {
-    dotenv().ok();
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
 
-    let admin_wallet_pk =
-        env::var("FAUCET_TESTNET_PRIVATE_KEY").expect("FAUCET_TESTNET_PRIVATE_KEY not set");
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(cli.quiet)
+        .verbosity(cli.verbosity as usize)
+        .timestamp(Timestamp::Millisecond)
+        .init()
+        .unwrap();
 
-    let port = env::var("FAUCET_PORT")
-        .ok()
-        .and_then(|port| port.parse().ok())
-        .unwrap_or(8081);
-
-    let register = warp::post()
-        .and(warp::path("register"))
-        .and(warp::header::exact("content-type", "application/json"))
-        .and(warp::body::json())
-        .and(with_private_key(admin_wallet_pk.clone()))
-        .and_then(handle_register);
-
-    let router = register
-        .with(
-            warp::cors()
-                .allow_any_origin()
-                .allow_headers(vec!["Content-Type"])
-                .allow_methods(vec!["POST"]),
-        )
-        .recover(handle_rejection);
-
-    warp::serve(router).run(([127, 0, 0, 1], port)).await;
-}
-
-#[derive(Deserialize)]
-struct RegisterRequest {
-    network: Network,
-    #[serde(deserialize_with = "deserialize_address")]
-    address: Address,
-}
-
-/// Custom deserializer to allow for FVM or EVM addresses to be used as input.
-fn deserialize_address<'de, D>(deserializer: D) -> Result<Address, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s: String = String::deserialize(deserializer)?;
-    parse_address(s.as_str()).map_err(serde::de::Error::custom)
-}
-
-/// Generic request error.
-#[derive(Clone, Debug)]
-struct BadRequest {
-    message: String,
-}
-
-impl warp::reject::Reject for BadRequest {}
-
-/// Custom error message with status code.
-#[derive(Clone, Debug, Serialize)]
-struct ErrorMessage {
-    code: u16,
-    message: String,
-}
-
-/// Rejection handler.
-async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
-    let (code, message) = if err.is_not_found() {
-        (StatusCode::NOT_FOUND, "Not Found".to_string())
-    } else if let Some(e) = err.find::<BadRequest>() {
-        let err = e.to_owned();
-        (StatusCode::BAD_REQUEST, err.message)
-    } else {
-        (StatusCode::INTERNAL_SERVER_ERROR, format!("{:?}", err))
-    };
-
-    let reply = warp::reply::json(&ErrorMessage {
-        code: code.as_u16(),
-        message,
-    });
-    Ok(warp::reply::with_status(reply, code))
-}
-
-/// Filter to pass the private key to the request handler.
-fn with_private_key(
-    private_key: String,
-) -> impl Filter<Extract = (String,), Error = std::convert::Infallible> + Clone {
-    warp::any().map(move || private_key.clone())
-}
-
-/// Get the admin wallet that will send transactions on the subnet.
-fn get_admin_wallet(private_key: &str, network: Network) -> anyhow::Result<Wallet> {
-    let pk = parse_secret_key(&private_key)?;
-    let signer = Wallet::new_secp256k1(pk, AccountKind::Ethereum, network.subnet_id()?)?;
-    Ok(signer)
-}
-
-/// Handles the `/register` request, first initializing the network.
-async fn handle_register(
-    req: RegisterRequest,
-    private_key: String,
-) -> Result<impl Reply, Rejection> {
-    req.network.init();
-    let res = register(req.address, req.network, &private_key)
-        .await
-        .map_err(|e| {
-            Rejection::from(BadRequest {
-                message: format!("register error: {}", e.to_string()),
-            })
-        })?;
-    let json = json!(res);
-    Ok(warp::reply::json(&json))
-}
-
-/// Registers an account on the subnet, creating the delegated EVM address (by
-/// transferring 0 FIL).
-async fn register(
-    address: Address,
-    network: Network,
-    private_key: &str,
-) -> anyhow::Result<TransactionReceipt, Box<dyn Error>> {
-    let signer = get_admin_wallet(private_key, network)?;
-    let config = network.subnet_config(Default::default())?;
-    let amount = TokenAmount::from_whole(0);
-    let tx = Account::transfer(&signer, address, config, amount).await?;
-    Ok(tx)
+    handle_server(cli).await
 }
